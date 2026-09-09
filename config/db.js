8becf3282833bee070d7359e7cabca48a68ecfb9
Prince Raj <princeraj12450@gmail.com>
2026-09-09 18:10:40 +0530

    Initial commit

---
+++ config/db.js
@@ -0,0 +1,36 @@
+// Day1
+// const mongoose = require('mongoose');
+
+// async function main() {
+//     await mongoose.connect(process.env.DB_CONNECT_STRING)
+// }
+
+
+// module.exports = main;
+
+import mongoose from "mongoose";
+
+let cached = global.mongoose
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null }
+}
+
+async function connectDB() {
+    if (cached.conn) {
+        return cached.conn
+    }
+    if(!cached.promise) {
+        const opts = {
+            bufferCommands: false,
+        }
+
+        cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/products`, opts).then((mongoose) => {
+            return mongoose
+        })
+    }
+
+    cached.conn = await cached.promise
+    return cached.conn
+}
+
+export default connectDB
