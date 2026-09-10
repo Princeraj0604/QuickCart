825361bcdc46d1356eb3141a697e0325f06be143
Prince Raj <princeraj12450@gmail.com>
2026-09-10 19:50:02 +0530

    Add seller product list API

---
+++ app/api/product/seller-list/route.js
@@ -0,0 +1,24 @@
+import authSeller from "@/lib/authSeller"
+import Product from "@/models/Product"
+import connectDB from "@/config/db"
+import { NextResponse } from "next/server"
+import { getAuth } from "@clerk/nextjs/server"
+
+export async function GET(request) {
+    try{
+
+        const { userId } = getAuth(request)
+        const isSeller = await authSeller(userId)
+        if(!isSeller) {
+            return NextResponse.json({success: false, message: "Unauthorized"}, {status: 401})
+        }
+
+        await connectDB()
+        const products = await Product.find({ userId }).sort({ date: -1 })
+        return NextResponse.json({success: true, products}, {status: 200})
+
+    }
+    catch(error){
+        return NextResponse.json({success: false, message: "Internal Server Error"}, {status: 500})
+    }
+}
